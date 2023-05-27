c2675a1d89c05bc00f8cc929535635574796c29c
agomolka <axrozwadowska@gmail.com>
2023-05-27 08:47:39 +0200

    Port update

--- app.py
+++ app.py
@@ -1,57 +1,61 @@
-import numpy as np
+
+import pickle
+from math import log10
+
 from flask import Flask
 from flask import request
 from flask import jsonify
-import pickle
+import numpy as np
 
-# Perceptron class implementation
 class Perceptron():
+    
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
-
+    
     def fit(self, X, y):
-        self.w_ = np.zeros(1 + X.shape[1])
+        self.w_ = np.zeros(1+X.shape[1])
         self.errors_ = []
-
+        
         for _ in range(self.n_iter):
             errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
+            for xi, target in zip(X,y):
+                update = self.eta*(target-self.predict(xi))
+                self.w_[1:] += update*xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
-
+    
     def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
+        return np.dot(X, self.w_[1:])+self.w_[0]
+    
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
-
+        return np.where(self.net_input(X)>=0.0,1,-1)
 
 # Create a flask
 app = Flask(__name__)
 
-
 # Create an API end point
-@app.route('/rta/v1.0/predict', methods=['GET'])
+@app.route('/api/v1.0/predict', methods=['GET'])
 def get_prediction():
+
+    # sepal length
     sepal_length = float(request.args.get('sl'))
     petal_length = float(request.args.get('pl'))
+    
+    features = [sepal_length,
+                petal_length]
 
-    features = [sepal_length, petal_length]
-
-    with open('model.pkl', "rb") as picklefile:
+    # Load pickled model file
+    with open('model.pkl',"rb") as picklefile:
         model = pickle.load(picklefile)
-
-    #predict the class using the model
+        
+    # Predict the class using the model
     predicted_class = int(model.predict(features))
-
-    #Json with featues and predictions
+    
+    # Return a json object containing the features and prediction
     return jsonify(features=features, predicted_class=predicted_class)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9999)
+    app.run(host='0.0.0.0', port=5000)
